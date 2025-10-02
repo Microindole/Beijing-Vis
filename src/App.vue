34e601680061cc30c4423db1c4ce87ac2938577f
程熙 张 <1563245867@qq.com>
2025-10-02 16:21:27 +0800

    initalize

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="visualization-system">
+    <header class="app-header">
+      <h1>北京城市时空演化可视分析系统</h1>
+    </header>
+
+    <div class="control-panel">
+    </div>
+
+    <main class="task-views-container">
+    </main>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue';
+
+
+</script>
+
+<style lang="css">
+@import './assets/styles/global.css';
+</style>
